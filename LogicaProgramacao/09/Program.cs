3b91e6f3b59ac8e85c49a3181b2fa4e55b62a2c7
Carol <carolduarte2000@gmail.com>
2024-09-17 18:36:27 -0300

    add activity #9

---
+++ LogicaProgramacao/09/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("=".PadLeft(23, '='));
+Console.WriteLine("Tabuada de Multiplicação");
+Console.WriteLine("=".PadLeft(23, '='));
+Console.ResetColor();
+
+int numero1;
+bool entradaValida;
+
+do
+{
+    Console.Write("Insira um número: ");
+    entradaValida = int.TryParse(Console.ReadLine(), out numero1);
+
+    if (!entradaValida)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Valor inválido. Por favor, insira um número inteiro.");
+        Console.ResetColor();
+    }
+
+} while (!entradaValida);
+
+for (int numero2 = 1; numero2 <= 10; numero2++)
+{
+    int resultado = numero1 * numero2;
+    Console.WriteLine($"{numero1} × {numero2} = {resultado}");
+}
