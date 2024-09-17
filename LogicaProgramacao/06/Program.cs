5a25dc57a0a40edcd47052b9702770b708472f03
Carol <carolduarte2000@gmail.com>
2024-09-17 18:20:34 -0300

    add activities #6 and #7

---
+++ LogicaProgramacao/06/Program.cs
@@ -0,0 +1,82 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("=".PadLeft(18, '='));
+Console.WriteLine("Calculadora Básica");
+Console.WriteLine("=".PadLeft(18, '='));
+Console.ResetColor();
+
+double numero1, numero2;
+bool entradaValida;
+
+do
+{
+    Console.Write("Digite o primeiro número: ");
+    entradaValida = double.TryParse(Console.ReadLine(), out numero1);
+
+    if (!entradaValida)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Número inválido. Tente novamente.");
+        Console.ResetColor();
+    }
+} while (!entradaValida);
+
+do
+{
+    Console.Write("Digite o segundo número: ");
+    entradaValida = double.TryParse(Console.ReadLine(), out numero2);
+
+    if (!entradaValida)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Número inválido. Tente novamente.");
+        Console.ResetColor();
+    }
+} while (!entradaValida);
+
+string operacao;
+double resultado = 0;
+entradaValida = false;
+
+do
+{
+    Console.Write("Escolha uma operação (+, -, *, /): ");
+    operacao = Console.ReadLine();
+
+    switch (operacao)
+    {
+        case "+":
+            resultado = numero1 + numero2;
+            entradaValida = true;
+            break;
+        case "-":
+            resultado = numero1 - numero2;
+            entradaValida = true;
+            break;
+        case "*":
+            resultado = numero1 * numero2;
+            entradaValida = true;
+            break;
+        case "/":
+            if (numero2 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Não existe divisão por zero. Tente novamente.");
+                Console.ResetColor();
+            }
+            else
+            {
+                resultado = numero1 / numero2;
+                entradaValida = true;
+            }
+            break;
+        default:
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Operação inválida. Tente novamente.");
+                Console.ResetColor();
+            }
+            break;
+    }
+} while (!entradaValida);
+
+Console.WriteLine($"O resultado é: {Math.Round(resultado, 2)}.");
---
+++ LogicaProgramacao/07/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("=".PadLeft(18, '='));
+Console.WriteLine("Conversor de Moeda");
+Console.WriteLine("=".PadLeft(18, '='));
+Console.ResetColor();
+
+double real;
+bool valorValido;
+
+do
+{
+    Console.Write("Insira o valor em reais: R$ ");
+
+    valorValido = double.TryParse(Console.ReadLine(), out real) && real > 0;
+
+    if (!valorValido)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Valor inválido. Por favor, insira um número maior do que 0.");
+        Console.ResetColor();
+    }
+
+} while (!valorValido);
+
+double dolar = 5.48, euro = 6.09, libra = 7.14;
+
+Console.WriteLine("Resultado da conversão para:");
+Console.WriteLine($"\t- Dólar: US$ {(real / dolar):0.00}");
+Console.WriteLine($"\t- Euro: € {(real / euro):0.00}");
+Console.WriteLine($"\t- Libra: £ {(real / libra):0.00}");
