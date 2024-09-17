2e8a64458367a9e58bcc4e4a878d9d3e900ca12d
Carol <carolduarte2000@gmail.com>
2024-09-17 14:39:42 -0300

    add activity #2

---
+++ LogicaProgramacao/02/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("=".PadLeft(28, '='));
+Console.WriteLine("Calculadora de Média Escolar");
+Console.WriteLine("=".PadLeft(28, '='));
+Console.ResetColor();
+
+double soma = 0;
+
+for (int i = 1; i <= 4; i++)
+{
+    double nota;
+    bool notaValida;
+
+    do
+    {
+        Console.Write($"Insira a {i}ª nota (de 0 a 100): ");
+        notaValida = double.TryParse(Console.ReadLine(), out nota) && nota >= 0 && nota <= 100;
+        if (!notaValida)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Valor inválido. Por favor, insira um número de 0 a 100.");
+            Console.ResetColor();
+        }
+    } while (!notaValida);
+
+    soma += nota;
+}
+
+double media = soma / 4;
+
+if (media >= 60)
+    Console.WriteLine($"O aluno está aprovado com média {media:F2}.");
+else if (media >= 40)
+    Console.WriteLine($"O aluno está de recuperação com média {media:F2}.");
+else
+    Console.WriteLine($"O aluno está reprovado com média {media:F2}.");
