00898f78ce7d354ee5e2bc47a47eede581df00ca
Carol <carolduarte2000@gmail.com>
2024-09-17 14:44:19 -0300

    add activity #3

---
+++ LogicaProgramacao/03/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("=".PadLeft(31, '='));
+Console.WriteLine("Verificador de Número Par-Ímpar");
+Console.WriteLine("=".PadLeft(31, '='));
+Console.ResetColor();
+
+int numero;
+bool numeroValido;
+
+do
+{
+    Console.Write("Informe um número: ");
+    numeroValido = int.TryParse(Console.ReadLine(), out numero);
+
+    if (!numeroValido)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Valor inválido. Por favor, insira um número inteiro.");
+        Console.ResetColor();
+    }
+} while (!numeroValido);
+
+if (numero % 2 == 0)
+    Console.WriteLine("O número informado é par.");
+else
+    Console.WriteLine("O número informado é ímpar.");
